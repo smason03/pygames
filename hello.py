3a6e7a040d3e176dfcea97ea6d8b0b4364a83782
swmason03 <swmason03@gmail.com>
2023-09-05 13:12:12 -0500

    <# Enter your message here. #>

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
 # This program say hello and asks for my name.
+#This is a new comment
 print('Hello World')
 print('What is your name?')
 myName = input()
 print('It is good to meet you, ' + myName)
+
+
+### Adding some more comments!!! 
